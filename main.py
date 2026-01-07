5161f96d65a22afac62e431abd6b2fcc621ee549
이진형 <ddocdoli@jbnu.ac.kr>
2026-01-07 15:23:34 +0900

    fix : WORKDIR 수정 + main.py에 tool import

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from mcp_container import mcp
 
-
 from starlette.middleware.cors import CORSMiddleware
-from mcp_container import app,base_app
 
-# if __name__ == "__main__":
-#     mcp.run(transport="streamable-http", 
-#             mount_path="/mcp", 
-#             host="localhost", 
-#             port=8000)
+from welfare_mcp.tools.user_profile import collect_basic_profile, collect_household_profile
+from welfare_mcp.tools.check_eligibility import check_eligibility
+from welfare_mcp.tools.required_documents import required_documents
+
+# from mcp_container import app
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http", 
+            mount_path="/mcp", 
+            host="localhost", 
+            port=8000)
 
 # Then wrap it with CORS middleware
-app = CORSMiddleware(
-    base_app,
-    allow_origins=["*"],  # Configure appropriately for production
-    allow_methods=["GET", "POST", "DELETE"],  # MCP streamable HTTP methods
-    expose_headers=["Mcp-Session-Id"],
-)
+# app = CORSMiddleware(
+#     base_app,
+#     allow_origins=["*"],  # Configure appropriately for production
+#     allow_methods=["GET", "POST", "DELETE"],  # MCP streamable HTTP methods
+#     expose_headers=["Mcp-Session-Id"],
+# )
