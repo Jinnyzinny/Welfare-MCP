79d218f9d3a2ee382ecf47971bdeef2d104e1ac4
이진형 <ddocdoli@jbnu.ac.kr>
2026-01-07 14:44:23 +0900

    fix : ASGI Server 설정 수정

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from mcp_container import mcp
 
 
-if __name__ == "__main__":
-    mcp.run(transport="streamable-http", 
-            mount_path="/mcp", 
-            host="localhost", 
-            port=8000)
+# if __name__ == "__main__":
+#     mcp.run(transport="streamable-http", 
+#             mount_path="/mcp", 
+#             host="localhost", 
+#             port=8000)
