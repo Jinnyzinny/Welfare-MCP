e1be807dc417bf5b977438f211969fa70cf0e8ab
이진형 <ddocdoli@jbnu.ac.kr>
2026-01-07 13:55:21 +0900

    fix : 표준 문제로 인한 fastpi-mcp에서 mcp SDK로 전환

--- mcp_container.py
+++ mcp_container.py
@@ -1,11 +1,40 @@
-# mcp_container.py
-import httpx
-from fastapi import FastAPI
-from fastapi_mcp import FastApiMCP
+import contextlib
 
-app = FastAPI()
-mcp = FastApiMCP(
-    app,
+# mcp_container.py
+from mcp.server.fastmcp import FastMCP
+
+from starlette.applications import Starlette
+from starlette.routing import Mount
+from starlette.middleware.cors import CORSMiddleware
+
+# Create an MCP server
+mcp = FastMCP(
     name="Welfare MCP Server",
-    description="MCP server for welfare services",
+    stateless_http=True,
+    json_response=True,
+)
+
+mcp_http_app = mcp.streamable_http_app()
+
+# Create a combined lifespan to manage both session managers
+# lifespan: 세션 매니저 필수
+@contextlib.asynccontextmanager
+async def lifespan(app: Starlette):
+    async with mcp.session_manager.run():
+        yield
+
+
+app=Starlette(
+    routes=[
+        Mount("/mcp", mcp_http_app) ,
+    ],
+    lifespan=lifespan,
+)
+
+# Then wrap it with CORS middleware
+starlette_app = CORSMiddleware(
+    app,
+    allow_origins=["*"],  # Configure appropriately for production
+    allow_methods=["GET", "POST", "DELETE"],  # MCP streamable HTTP methods
+    expose_headers=["Mcp-Session-Id"],
 )
