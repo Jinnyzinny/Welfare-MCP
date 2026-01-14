a6a5812bb32170c4450ea30f5350aae222821a16
이진형 <ddocdoli@jbnu.ac.kr>
2026-01-14 13:31:08 +0900

    fix : DTO 삭제

--- welfare_mcp/backend/dto/request/CollectAssetProfileReqeustDto.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from backend.entity.UserProfile import UserProfile
-
-class CollectAssetProfileRequestDto(BaseModel):
-    current_profile: UserProfile
-    has_real_estate: bool
-    has_vehicle: bool
