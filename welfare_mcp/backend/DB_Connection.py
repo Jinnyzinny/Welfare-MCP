c3aa7de35eaa7ea32f85fce346d78250f6800811
이진형 <ddocdoli@naver.com>
2026-02-03 14:22:45 +0900

    fix : batch 작업을 비동기로 전환

--- welfare_mcp/backend/DB_Connection.py
+++ welfare_mcp/backend/DB_Connection.py
@@ -1,21 +1,20 @@
-import psycopg2
+import asyncio
 import os
 
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST=os.getenv("DB_HOST")
-DB_PORT=os.getenv("DB_PORT")
-DB_NAME=os.getenv("DB_NAME")
-DB_USERNAME=os.getenv("DB_USERNAME")
-DB_PASSWORD=os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
 # 데이터베이스 연결 설정
-def dbConn():
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USERNAME,
-        password=DB_PASSWORD
+async def dbConn():
+    conn = await asyncio.connect(
+        host=DB_HOST, database=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD
     )
     return conn
